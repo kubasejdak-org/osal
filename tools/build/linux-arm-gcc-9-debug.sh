0f5c39b788f8fb53f17693eb9a6e07fda7186f44
Kuba Sejdak <kuba.sejdak@gmail.com>
2020-02-22 21:15:47 +0100

    Use gcc-9 again.

--- tools/build/linux-arm-gcc-9-debug.sh
+++ tools/build/linux-arm-gcc-9-debug.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-conan install .. --build missing -pr arm-linux-gnueabihf-gcc-8 -s build_type=Debug
-cmake .. -DPLATFORM=linux-arm -DTOOLCHAIN=arm-linux-gnueabihf-gcc-8 "${@}"
+conan install .. --build -pr arm-linux-gnueabihf-gcc-9 -s build_type=Debug
+cmake .. -DPLATFORM=linux-arm "${@}"
