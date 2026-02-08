89637e32c560d472b1438e7e89182c0ccee63507
Kuba Sejdak <kuba.sejdak@gmail.com>
2026-02-08 22:48:39 +0100

    Update platform GIT_TAG in Findplatform.cmake to match latest commit

--- cmake/modules/Findplatform.cmake
+++ cmake/modules/Findplatform.cmake
@@ -1,7 +1,7 @@
 include(FetchContent)
 FetchContent_Declare(platform
     GIT_REPOSITORY  https://github.com/kubasejdak-org/platform.git
-    GIT_TAG         9c7fc70d20395a29254a0c12d4276cd13a542bed
+    GIT_TAG         dea2a14ea9acdde6a258b055b6b5a9ec0791e7b9
     SOURCE_SUBDIR   lib
 )
 
