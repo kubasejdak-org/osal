384ec62bb979cd4b49d55da86b579ae98e91150c
Kuba Sejdak <kuba.sejdak+github@gmail.com>
2026-03-01 00:08:24 +0100

    Update platform dependency to latest commit and change copyright license in main.cpp (#11)

--- cmake/modules/Findplatform.cmake
+++ cmake/modules/Findplatform.cmake
@@ -1,7 +1,7 @@
 include(FetchContent)
 FetchContent_Declare(platform
     GIT_REPOSITORY  https://github.com/kubasejdak-org/platform.git
-    GIT_TAG         79999ea692762b9aeaca3714f9f1e004add14506
+    GIT_TAG         a3b3a46898c4ff4d6e379b7383e4c82b07f05413
     SOURCE_SUBDIR   lib
 )
 
