d4d6f5b6c021a19b3b08c7604df5f0ea3ee3b2e7
Kuba Sejdak <kuba.sejdak@gmail.com>
2026-03-07 21:21:34 +0100

    Use new component system

--- cmake/modules/Findosal.cmake
+++ cmake/modules/Findosal.cmake
@@ -1,4 +1,2 @@
-# set(osal_SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}/../..)
-# include(${CMAKE_CURRENT_LIST_DIR}/../components.cmake)
-
-set(stm32f4xx_FOUND TRUE)
+set(osal_SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}/../..)
+include(${CMAKE_CURRENT_LIST_DIR}/../components.cmake)
