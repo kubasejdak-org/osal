0b03f701d8e71f294000f7816aa05bf42e2150ea
Kuba Sejdak <kuba.sejdak@gmail.com>
2026-04-22 22:56:44 +0200

    fix: update GIT_TAG for platform dependency in Findplatform.cmake

--- cmake/modules/Findplatform.cmake
+++ cmake/modules/Findplatform.cmake
@@ -1,7 +1,7 @@
 include(FetchContent)
 FetchContent_Declare(platform
     GIT_REPOSITORY  https://github.com/kubasejdak-org/platform.git
-    GIT_TAG         be249886775d8c889c2cf324b736bf6da028defd
+    GIT_TAG         1fa3f099b16b35185507fce55946073a0d75d4e7
     SOURCE_SUBDIR   lib
 )
 
