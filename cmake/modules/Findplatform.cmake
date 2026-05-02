95eae01ed6e1baf51d4f7a745a9416a1d7ac9fb7
Kuba Sejdak <kuba.sejdak@gmail.com>
2026-05-02 23:08:33 +0200

    fix: update platform GIT_TAG and enhance linker script for memory section definitions

--- cmake/modules/Findplatform.cmake
+++ cmake/modules/Findplatform.cmake
@@ -1,7 +1,7 @@
 include(FetchContent)
 FetchContent_Declare(platform
     GIT_REPOSITORY  https://github.com/kubasejdak-org/platform.git
-    GIT_TAG         db273b749620432ea33300cce10d2431fb1db1d4
+    GIT_TAG         b3a55804506e07e320f0cd47a28bc3f6902d50ca
     SOURCE_SUBDIR   lib
 )
 
